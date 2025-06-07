d824daadb7de80df94d2b74dffa54a7a4bc4dcb3
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2025-06-07 10:44:38 +0700

    Initial Commit

---
+++ BitMEXOrderBook/BitMEXOrderBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitMEXOrderBookApp.swift
+//  BitMEXOrderBook
+//
+//  Created by Khanh Nguyen on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BitMEXOrderBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitMEXOrderBookTests/BitMEXOrderBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitMEXOrderBookTests.swift
+//  BitMEXOrderBookTests
+//
+//  Created by Khanh Nguyen on 7/6/25.
+//
+
+import Testing
+@testable import BitMEXOrderBook
+
+struct BitMEXOrderBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
